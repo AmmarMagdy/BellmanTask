3589def4f27ac8313141603766815e36e7cc25d3
Ammar Magdy <5722698-Ammar.M@users.noreply.gitlab.com>
2021-03-20 15:37:57 +0200

    add header category cell

--- BellmanTask/UI/Home/Controller/HomeViewController.swift
+++ BellmanTask/UI/Home/Controller/HomeViewController.swift
@@ -7,21 +7,55 @@
 
 import UIKit
 
+enum itemSections {
+    case hotspots
+    case events
+    case attractions
+}
+
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
     private let categoryCellIdentifier = "categoryCellIdentifier"
+    private let sectionHeaderIdentifier = "sectionHeaderIdentifier"
+    var itemSections : [itemSections] = [.hotspots, .events, .attractions]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupRegisterCollectionView()
+    }
+    
+    func setupRegisterCollectionView() {
+        collectionView.register(UINib.init(nibName: "HomeHeaderReusableView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: sectionHeaderIdentifier)
+        let nib = UINib(nibName: "HorizontalCollectionCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: categoryCellIdentifier)
     }
 
 }
 
 extension HomeViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: sectionHeaderIdentifier, for: indexPath) as! HomeHeaderReusableView
+        switch itemSections[indexPath.section]{
+        case .hotspots:
+            headerView.titleLabel.text = "Hotspots"
+        case .events:
+            headerView.titleLabel.text = "Events"
+        case .attractions:
+            headerView.titleLabel.text = "Attractions"
+        }
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width = Double(collectionView.bounds.width) - 30
+        let height: Double = Double(self.view.bounds.height * 0.05)
+        return CGSize(width: width, height: height)
+    }
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 3
---
+++ BellmanTask/UI/Home/View/HomeHeaderCell/HomeHeaderReusableView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeCollectionReusableView.swift
+//  Happy Days
+//
+//  Created by Ammar.M on 2/16/20.
+//  Copyright © 2020 Afnan. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderReusableView: UICollectionReusableView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
