ddc84411644cd56a915495dd248d23965ce0f85e
Ammar Magdy <5722698-Ammar.M@users.noreply.gitlab.com>
2021-03-20 13:47:43 +0200

    first commit

---
+++ BellmanTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BellmanTask
+//
+//  Created by Ammar.M on 20/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
